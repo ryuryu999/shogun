b24a2854478f9d36febb829b4dd4d2a661a1b113
ryuryu999 <family21456@yashoo.co.jp>
2018-05-05 23:38:22 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('H2BhCCb4wm8dyyosTi3bp6fDfBasBR6RFKA6O3X8Qs79wL/xyft/9MsBSQF0koROOnJk0iB0HPLJ+KPV/u/srhMhHzR9czxmmC/cnwAKLqEzIbakxZw1rEfvXDn3PhteF/hzMhm13U17rJBVTKiTZQdB04t89/1O/w1cDnyilFU=
+')
+handler = WebhookHandler('95284773b5135e7accf6cefe4a8f695e')
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
