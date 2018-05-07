e721402ef2d77d7f17ef076406acc420dbeb18aa
ryuryu999 <family21456@yashoo.co.jp>
2018-05-08 00:02:12 +0900

    first commit Yeaahhhh

--- app.py
+++ app.py
@@ -1,43 +1,48 @@
 from flask import Flask, request, abort
 
 from linebot import (
- LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
- InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
- MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
-# 自分のLINE botのデータを設定する。
-line_bot_api = LineBotApi('H2BhCCb4wm8dyyosTi3bp6fDfBasBR6RFKA6O3X8Qs79wL/xyft/9MsBSQF0koROOnJk0iB0HPLJ+KPV/u/srhMhHzR9czxmmC/cnwAKLqEzIbakxZw1rEfvXDn3PhteF/hzMhm13U17rJBVTKiTZQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('4a3b7434e9f34fae934518c3d4f7aa9e')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
- # get X-Line-Signature header value
- signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-# get request body as text
- body = request.get_data(as_text=True)
- app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-# handle webhook body
- try:
-  handler.handle(body, signature)
- except InvalidSignatureError:
-  abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
- return 'OK'
+    return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
- line_bot_api.reply_message(
-  event.reply_token,
-  TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
- app.run()
+    app.run()
